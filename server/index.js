53dba58f38e37838aedebdf19ce05fefc1fd6401
MG <coalsrbs7@gmail.com>
2023-12-18 22:37:16 -0300

    first commit

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0434d2f70234139f780f2b40f79a79f76346778adf0aef7a96d1e98ae819a3067e7f72eae10befa27eb3797a42d2e3a0abe5c830be6a4c1b1bb2f13000d8f0e70d": 100,
+  "04fcc52d923f686fb5298e1d94acaa2cd20deb01d35e19d0d5bada5762ded990af8d143263640164978834df5fd4e8179673803199a5fc327eea1eb0adc559ab5c": 50,
+  "04b8a54642ec30088a575bd943bc2380b65f3f192fd7272424a4b1cc7ab51b18bc7a2fc23c7daf6087d9aea193d3d1aeff9ad6cd157ac339ee067ead525f8692da": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
